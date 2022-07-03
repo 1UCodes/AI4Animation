2d7a71f34a32d574a96f2f11667c39174d767112
Juan Belon <juaxix@gmail.com>
2022-07-03 23:06:37 +0200

    New Player controller. Updated modules. Removed NNController BP

---
+++ AI4Animation/SIGGRAPH_2017/Unreal/Plugins/DeepLearningHelpers/Source/DeepLearningHelpers/Private/DL_PlayerController.cpp
@@ -0,0 +1,5 @@
+// juaxix - 2020-2022 - xixgames
+
+
+#include "DL_PlayerController.h"
+
---
+++ AI4Animation/SIGGRAPH_2017/Unreal/Plugins/DeepLearningHelpers/Source/DeepLearningHelpers/Public/DL_PlayerController.h
@@ -0,0 +1,17 @@
+// juaxix - 2020-2022 - xixgames
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DL_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEEPLEARNINGHELPERS_API ADL_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
