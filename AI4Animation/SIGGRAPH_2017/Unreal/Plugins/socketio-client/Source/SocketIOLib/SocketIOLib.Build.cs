e0e736ed3e788d28d5ae3c88cee5592133312fc8
Juan Belon <juaxix@gmail.com>
2022-07-20 21:33:10 +0100

    Deiniting submodules for tensorflow, machine learning and socketio. Using the code directly as I made changes on them.

---
+++ AI4Animation/SIGGRAPH_2017/Unreal/Plugins/socketio-client/Source/SocketIOLib/SocketIOLib.Build.cs
@@ -0,0 +1,66 @@
+// Copyright 2018-current Getnamo. All Rights Reserved
+
+using UnrealBuildTool;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Tools.DotNETCommon;
+
+namespace UnrealBuildTool.Rules
+{
+	public class SocketIOLib : ModuleRules
+	{
+		private string ThirdPartyPath
+        {
+            get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "..", "ThirdParty")); }
+        }
+
+	    public SocketIOLib(ReadOnlyTargetRules Target) : base(Target)
+	    {
+			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+			bUseRTTI = true;
+			bEnableExceptions = true;
+
+			PublicIncludePaths.AddRange(
+	            new string[] {
+					Path.Combine(ModuleDirectory, "Public"),
+	            }
+	            );
+
+
+	        PrivateIncludePaths.AddRange(
+	            new string[] {
+					Path.Combine(ModuleDirectory, "Private"),
+					Path.Combine(ModuleDirectory, "Private/internal"),
+					Path.Combine(ThirdPartyPath, "websocketpp"),
+					Path.Combine(ThirdPartyPath, "asio/asio/include"),
+					Path.Combine(ThirdPartyPath, "rapidjson/include"),
+	            }
+	            );
+
+
+	        PublicDependencyModuleNames.AddRange(
+	            new string[]
+	            {
+	            "Core",
+	            }
+	            );
+
+
+	        PrivateDependencyModuleNames.AddRange(
+	            new string[]
+	            {
+	            "CoreUObject",
+	            "Engine",
+	            }
+	            );
+
+
+	        DynamicallyLoadedModuleNames.AddRange(
+	            new string[]
+	            {
+	            }
+	            );
+	    }
+	}
+}
---
+++ AI4Animation/SIGGRAPH_2017/Unreal/Plugins/tensorflow/Source/TensorFlow/Public/TensorFlow.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Modules/ModuleInterface.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(TensorFlowLog, Log, All);
+
+class FTensorFlowModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
